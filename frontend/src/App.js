2005a15a467147be3b357202467299299caa8e98
Jenny553 <164994628+Jenny553@users.noreply.github.com>
2024-04-28 01:45:53 +0400

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import './App.css'
 import Navbar from './components/Navbar/navbar';
-
-
 function App() {
   return (
     <div>
-      
-      <Navbar/>
-   
+     
+     <Navbar/>
+     
     </div>
   );
 }

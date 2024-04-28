11257039d85c035e682719a2ce116167d7c6479f
Jenny553 <164994628+Jenny553@users.noreply.github.com>
2024-04-28 06:24:24 +0400

    nav&side

--- admin/src/components/sidebar/sidebar.jsx
+++ admin/src/components/sidebar/sidebar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './sidebar.css'
+import add_product_icon from '../../assets/Product_Cart.svg'
+import list_product_icon from '../../assets/Product_list_icon.svg'
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="sidebar-item">
                 <Link to={"/addproduct"} style={textDecoration= "none"}>
+                    <img src={add_product_icon} alt="" />
                     <div>
                         <p>Add Product</p>
                     </div>
@@ -13,6 +17,7 @@ const Sidebar = () => {
             </div>
             <div className="sidebar-item">
                 <Link to={"/listproduct"} style={textDecoration= "none"}>
+                    <img src={list_product_icon} alt="" />
                     <div>
                         <p>Product List</p>
                     </div>

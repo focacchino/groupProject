0b7ed5a70164b14e6c9a709e0c91573dcdc0e115
Jenny553 <164994628+Jenny553@users.noreply.github.com>
2024-05-09 10:05:17 +0400

    Add files via upload

---
+++ frontend/src/Components/AdminPanel/Sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Sidebar.css';
+import add_product_icon from '../../Assets/product_cart_icon.png';
+import list_product_icon from '../../Assets/product_list_icon.png';
+
+
+const Sidebar = () => {
+    return (
+        
+            <div className="sidebar">
+                <Link to={'/addproduct'} style={{textDecoration:"none"}}>
+                    <div className="sidebar-item">
+                        <img src={add_product_icon} alt="" />
+                        <p>Add Product</p>
+                    </div>
+                </Link>
+                <Link to={'/listproduct'} style={{textDecoration:"none"}}>
+                    <div className="sidebar-items">
+                        <img src={list_product_icon} alt="" />
+                        <p>Product List</p>
+                    </div>
+                </Link>
+            </div>
+    );
+}
+
+export default Sidebar;

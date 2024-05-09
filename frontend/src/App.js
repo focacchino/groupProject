6f01cc95cbcc9e66b611075253892dc098bd72b7
hehe <150682039+focacchino@users.noreply.github.com>
2024-05-09 20:31:48 +0530

    fixing changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter,Routes,Route} from 'react-router-dom'
-import Shop from './Pages/Shop';
+// import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
@@ -18,7 +18,7 @@ function App() {
       <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Shop/>}/>
+        {/* <Route path='/' element={<Shop/>}/> */}
         <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />}/>
         <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
